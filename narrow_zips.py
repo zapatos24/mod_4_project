9a8f1e54c485bb00fe4594780db83418b875e41e
Jeremy Owens <jeremy.traber.owens@gmail.com>
2019-07-29 13:36:43 -0400

    in the middle of troubleshooting code base join

--- narrow_zips.py
+++ narrow_zips.py
@@ -2,38 +2,37 @@ import csv
 import pandas as pd
 import numpy as np
 import datetime
+from ProcessData import ProcessData
 
-start_year = 2012
-filename = 'data.csv'
+
+start_date = '2010-01-01'
 pct_change_filter = .01
 std_dev_filter = .33
-cols = ['Zipcode', 'City', 'State', 'Pct_change', 'Std_dev']
+cols = ['Zipcode', 'City', 'State', 'Pct_change', 'Std_dev', 'Beta']
 
+def filter_data(dataframes):
+    filtered_dfs = []
+    for dataframe in dataframes:
+        filtered_df = dataframe.loc[start_date:]
+        filtered_dfs.append(filtered_df)
+    return filtered_dfs
 
-def prep_data(filename, start_year):
-    raw_data = pd.io.parsers.read_csv(filename, dtype={'Zipcode': 'str'}, index_col=[0])
-    raw_data.index = pd.to_datetime(raw_data.index)
-    start_date = datetime.datetime(start_year, 1, 1)
-    data = raw_data.loc['2012-01-01':]
-    return data
+def diff_month(date1, date2):
+    return abs((date1.year - date2.year) * 12 + date1.month - date2.month)
 
-def get_stats(all_zipcodes):
+def get_stats(dataframes):
     zipcode_stats = []
-    for curr_zipcode in all_zipcodes:
-        zipcode_data = data[data.Zipcode == curr_zipcode]
-        price_diff = zipcode_data.iloc[-1].price - zipcode_data.iloc[0].price
-        pct_change = price_diff / (zipcode_data.iloc[0].price * 100)
-        std_dev = np.std(zipcode_data.price)
-        curr_zip_stats = [zipcode_data.iloc[0].Zipcode, zipcode_data.iloc[0].City,
-                          zipcode_data.iloc[0].State, pct_change, std_dev]
+    for df in dataframes:
+        price_diff = df.iloc[-1].Price - df.iloc[0].Price
+        pct_change = (price_diff / df.iloc[0].Price) * 100
+        std_dev = np.std(df.Price)
+        variance = std_dev ** 2
+        covariance = np.cov(df.Price, monthly_medians.values)[0][1]
+        beta = covariance/variance
+        curr_zip_stats = [df.iloc[0].Zipcode, df.iloc[0].City,
+                          df.iloc[0].State, pct_change, std_dev, beta]
         zipcode_stats.append(curr_zip_stats)
     return zipcode_stats
-
-# data.csv is output from process_data.py
-
-data = prep_data(filename, start_year)
-all_zipcodes = data.Zipcode.unique()
-zipcode_stats = get_stats(all_zipcodes)
 
 def get_top_zipcodes(zipcode_stats):
     sorted_by_pct_change = sorted(zipcode_stats, key=lambda x: x[3], reverse=True)
@@ -43,17 +42,18 @@ def get_top_zipcodes(zipcode_stats):
     top_zipcodes = sorted(lowest_std_devs, key=lambda x: x[3], reverse=True)
     return pd.DataFrame(top_zipcodes, columns=cols)
 
-top_zipcodes = get_top_zipcodes(zipcode_stats)
-
 def compare_top_zipcodes(top_zipcodes):
     top_states = top_zipcodes.State.unique()
     already_used = dict(zip(top_states, [False for _ in range(len(top_states))]))
     zipcodes_to_model = []
+    #  ensures only 1 zipcode per state
     for row in top_zipcodes.iterrows():
         if not already_used[row[1].State]:
             zipcodes_to_model.append(row[1])
             already_used[row[1].State] = True
     return pd.DataFrame(zipcodes_to_model, columns=cols)
 
-zipcodes_to_model = compare_top_zipcodes(top_zipcodes)
-zipcodes_to_model
+
+# zipcode_stats = get_stats(dataframes)
+# top_zipcodes = get_top_zipcodes(zipcode_stats)
+# zipcodes_to_model = compare_top_zipcodes(top_zipcodes)
