ed537c59978c7a61e5875410e2914c1ed7626358
Jeremy Reikes <jeremyreikes@gmail.com>
2019-07-28 21:24:06 -0400

    'changing data to list of dfs'

--- narrow_zips.py
+++ narrow_zips.py
@@ -3,36 +3,42 @@ import pandas as pd
 import numpy as np
 import datetime
 
-start_year = 2012
-filename = 'data.csv'
+start_date = '2010-01-01'
 pct_change_filter = .01
 std_dev_filter = .33
-cols = ['Zipcode', 'City', 'State', 'Pct_change', 'Std_dev']
+filename = 'data.csv'
+cols = ['Zipcode', 'City', 'State', 'Pct_change', 'Std_dev', 'Beta']
 
+def diff_month(date1, date2):
+    return abs((date1.year - date2.year) * 12 + date1.month - date2.month)
 
 def prep_data(filename, start_year):
     raw_data = pd.io.parsers.read_csv(filename, dtype={'Zipcode': 'str'}, index_col=[0])
     raw_data.index = pd.to_datetime(raw_data.index)
-    start_date = datetime.datetime(start_year, 1, 1)
-    data = raw_data.loc['2012-01-01':]
+    data = raw_data.loc[start_date:]
     return data
 
 def get_stats(all_zipcodes):
     zipcode_stats = []
-    for curr_zipcode in all_zipcodes:
-        zipcode_data = data[data.Zipcode == curr_zipcode]
-        price_diff = zipcode_data.iloc[-1].price - zipcode_data.iloc[0].price
-        pct_change = price_diff / (zipcode_data.iloc[0].price * 100)
-        std_dev = np.std(zipcode_data.price)
+    rows_per_zip = int(len(data) / data.Zipcode.nunique())
+    for index, curr_zipcode in enumerate(all_zipcodes):
+        start = index * rows_per_zip
+        zipcode_data = data.iloc[start: start + rows_per_zip]
+        price_diff = zipcode_data.iloc[-1].Price - zipcode_data.iloc[0].Price
+        pct_change = (price_diff / zipcode_data.iloc[0].Price) * 100
+        std_dev = np.std(zipcode_data.Price)
+        variance = std_dev ** 2
+        covariance = np.cov(zipcode_data.Price, monthly_medians.values)[0, 1]
+        beta = covariance/variance
         curr_zip_stats = [zipcode_data.iloc[0].Zipcode, zipcode_data.iloc[0].City,
-                          zipcode_data.iloc[0].State, pct_change, std_dev]
+                          zipcode_data.iloc[0].State, pct_change, std_dev, beta]
         zipcode_stats.append(curr_zip_stats)
     return zipcode_stats
 
 # data.csv is output from process_data.py
-
 data = prep_data(filename, start_year)
 all_zipcodes = data.Zipcode.unique()
+monthly_medians = data.groupby('Time').agg({'Price': 'median'}).loc[start_date:]
 zipcode_stats = get_stats(all_zipcodes)
 
 def get_top_zipcodes(zipcode_stats):
@@ -44,16 +50,19 @@ def get_top_zipcodes(zipcode_stats):
     return pd.DataFrame(top_zipcodes, columns=cols)
 
 top_zipcodes = get_top_zipcodes(zipcode_stats)
-
+top_zipcodes
 def compare_top_zipcodes(top_zipcodes):
     top_states = top_zipcodes.State.unique()
     already_used = dict(zip(top_states, [False for _ in range(len(top_states))]))
     zipcodes_to_model = []
+    #  ensures only 1 zipcode per state
     for row in top_zipcodes.iterrows():
         if not already_used[row[1].State]:
             zipcodes_to_model.append(row[1])
             already_used[row[1].State] = True
     return pd.DataFrame(zipcodes_to_model, columns=cols)
 
+
+# 2004-2010
 zipcodes_to_model = compare_top_zipcodes(top_zipcodes)
 zipcodes_to_model
--- process_data.py
+++ process_data.py
@@ -3,37 +3,45 @@ import numpy as np
 import datetime
 
 # changes data from wide format to long format
-def melt_data(df):
-    melted = pd.melt(df, id_vars=['RegionName', 'City', 'State', 'Metro', 'CountyName', 'RegionID', 'SizeRank'], var_name='time')
-    melted['time'] = pd.to_datetime(melted['time'], infer_datetime_format=True)
-    melted.set_index('time', inplace=True)
-    melted.drop('RegionID', inplace=True, axis=1)
-    melted.rename(columns = {'RegionName':'Zipcode', 'value':'price'}, inplace=True)
-    return melted
+class Process_data:
+    def __init__(self, filename):
+        raw_data = pd.io.parsers.read_csv('zillow_data.csv', dtype={'RegionName': 'str', 'Price': 'int'})
+        melted = melt_data(raw_data)
 
-# backfills (estimates) a zipcode's historical price according to avg % change in housing market
-def backfill_price(group):
-    for i in range(len(group) - 1):
-        if np.isnan(group.iloc[-1-i]['price']):
-            base_price = group.iloc[-i]['price']
-            growth_rate = monthly_medians.iloc[-1-i] / monthly_medians.iloc[-i]
-            group.iloc[-1-i]['price'] = (base_price * growth_rate)
+    def melt_data(df):
+        melted = pd.melt(df, id_vars=['RegionName', 'City', 'State', 'Metro', 'CountyName', 'RegionID', 'SizeRank'], var_name='time')
+        melted.rename(columns = {'RegionName':'Zipcode', 'value':'Price', 'time':'Time'}, inplace=True)
+        melted['Time'] = pd.to_datetime(melted['Time'], infer_datetime_format=True)
+        melted.set_index('Time', inplace=True)
+        melted.drop('RegionID', inplace=True, axis=1)
+        return melted
 
-# returns dataframe with every zipcode fully backfilled
-def process_data(data):
-    groups = data.groupby('Zipcode')
-    groups.apply(backfill_price)
-    dataframes = []
-    for name, group in groups:
-        dataframes.append(group)
-    return pd.concat(dataframes)
+    # backfills (estimates) a zipcode's historical price according to avg % change in housing market
+    def backfill_price(group):
+        prices = []
+        for i in range(len(group)):
+            curr_price = group.iloc[-1-i]['Price']
+            if np.isnan(curr_price):
+                base_price = prices[i-1]
+                growth_rate = monthly_medians.iloc[-1-i] / monthly_medians.iloc[-i]
+                prices.append(int(base_price * growth_rate))
+            else:
+                prices.append(int(curr_price))
+        return reversed(prices)
 
-# 'zillow_data.csv' contains average monthly housing prices in USA by zipcode
-raw_data = pd.io.parsers.read_csv('zillow_data.csv', dtype={'RegionName': 'str'})
-data = melt_data(raw_data)
-monthly_medians = data.groupby('time').agg({'price':'median'})
-data.sort_values(by=['Zipcode', 'time'], ascending=True, inplace=True)
+    # 'zillow_data.csv' contains average monthly housing prices in USA by zipcode
 
-# uncomment next two lines to process and save data for analysis (takes ~30 minutes)
-# all_data = process_data(data)
-# all_data.to_csv('data.csv')
+    monthly_medians = melted.groupby('Time').agg({'Price':'median'})
+    groups = melted.groupby('Zipcode')
+    data = []
+    for group in groups:
+        prices = backfill_price(group)
+        group['Prices'] = prices
+        data.append(group)
+
+    f = open(filename, 'a')
+    for df in l1:
+        df.to_csv(f)
+    f.close()
+
+    data.to_csv('data.csv')
