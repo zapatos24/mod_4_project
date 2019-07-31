59156191b287a588992a993be393d44b326a76f5
Jeremy Owens <jeremy.traber.owens@gmail.com>
2019-07-31 18:24:05 -0400

    removing unnecessary code from wip

--- arima_model.py
+++ arima_model.py
@@ -5,6 +5,7 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 import warnings
 
+
 def evaluate_arima_model(time_series, arima_order):
     # prepare training dataset
     time_series_post_crash = time_series['2010':].squeeze()
@@ -23,7 +24,8 @@ def evaluate_arima_model(time_series, arima_order):
     # calculate out of sample error
     error = mean_squared_error(test, predictions)
     return error
-# %%
+
+
 # evaluate combinations of p, d and q values for an ARIMA model
 def evaluate_models(dataset, p_values, d_values, q_values):
     dataset = dataset.astype('float32')
@@ -40,16 +42,17 @@ def evaluate_models(dataset, p_values, d_values, q_values):
                     continue
     print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
     return best_cfg
-# %%
-warnings.filterwarnings("ignore")
-p_values = range(0, 4)
-d_values = range(0, 3)
-q_values = range(0, 3)
-best_post_crash_arima = []
-for index in top_indexes[:3]:
-    print('Index:', index, 'Zipcode:', zipcodes[index])
-    pdq = evaluate_models(time_series[index], p_values, d_values, q_values)
-    print()
-    best_post_crash_arima.append({'index': index, 'zipcode': zipcodes[index], 'pdq': pdq})
-# %%
-best_post_crash_arima
+
+
+# warnings.filterwarnings("ignore")
+# p_values = range(0, 4)
+# d_values = range(0, 3)
+# q_values = range(0, 3)
+# best_post_crash_arima = []
+# for index in top_indexes[:3]:
+#     print('Index:', index, 'Zipcode:', zipcodes[index])
+#     pdq = evaluate_models(time_series[index], p_values, d_values, q_values)
+#     print()
+#     best_post_crash_arima.append({'index': index, 'zipcode': zipcodes[index], 'pdq': pdq})
+# # %%
+# best_post_crash_arima
